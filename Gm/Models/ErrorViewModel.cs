a18a0d26a40bb99d18100073f4c890b1282d863a
NhomNhem <95702943+NhomNhem@users.noreply.github.com>
2022-11-05 14:12:07 +0700

    Add project files.

---
+++ Gm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
